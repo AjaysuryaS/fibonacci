8d4adc431d08a39e32eee010ffbb34dcdc55ee5f
AjaysuryaS <suriyasajai16@gmail.com>
2023-08-10 12:35:48 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int inputNumber = 6; // Change this to the desired Fibonacci sequence index
+        FibonacciFinder fibonacciFinder = new FibonacciFinder();
+        System.out.println(fibonacciFinder.fibonacci(inputNumber));
+    }
+}
+
+class FibonacciFinder {
+    int fibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
